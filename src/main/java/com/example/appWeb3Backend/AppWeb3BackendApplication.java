acf646ec569719a1a33af5f0c8702924b135292d
CesarCejaC <tonycejacortes@gmail.com>
2022-03-11 19:22:51 -0600

    initial comit

---
+++ src/main/java/com/example/appWeb3Backend/AppWeb3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appWeb3Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWeb3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWeb3BackendApplication.class, args);
+	}
+
+}
